84ea75d1f6fa643c9c29d4460b8c7085ad6ca832
David Zarate <dzlopez35@gmail.com>
2021-04-27 16:20:17 -0500

    Adding files

---
+++ database/migrations/20210427210615-create-curso.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Cursos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING
+      },
+      descripcion: {
+        type: Sequelize.STRING
+      },
+      nivel: {
+        type: Sequelize.STRING
+      },
+      categoria: {
+        type: Sequelize.STRING
+      },
+      duracion: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Cursos');
+  }
+};
